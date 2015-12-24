4f84b0b449bf6b9544618000b9006394f3a5cff6
саша <enik@macbook-pro-ar0n.local>
2015-12-25 02:07:45 +0300

    Initial Commit

---
+++ gitChess/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitChess
+//
+//  Created by Admin on 25.12.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitChess/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitChess
+//
+//  Created by Admin on 25.12.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
